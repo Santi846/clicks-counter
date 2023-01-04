65e7b38c70e84aa483504aea54e5510948b2ea14
Santiago Arocha <arochas846@gmail.com>
2023-01-03 21:03:49 -0300

    Start the project and prepare the background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Kakashi_Purple_Lightning from './images/Kakashi_Purple_Lightning.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <h1 className='title'>Clicks Counter</h1> */}
+      <div className='background_container'>
+        <img className='background' src={Kakashi_Purple_Lightning} alt='background' />
+      </div>
+      <div className='main_container'></div>
     </div>
   );
 }
