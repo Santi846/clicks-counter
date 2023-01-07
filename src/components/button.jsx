acc21d8a66750db8b5be6a044203f41a30576310
Santiago Arocha <arochas846@gmail.com>
2023-01-07 20:45:22 -0300

    console cleaning

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -5,6 +5,7 @@ export function Button({text, is_button_click, manageClick, restartCounter }) {
     return (
         <button 
         className={ is_button_click ? 'button_click' : 'button_restart' }
+        // TODO: Try this -> 'return a Promise from the event listener, and resolve when you have the response (or reject it in case of an error).' for taking out the async uncaught error for waiting for onclick event
         onClick={is_button_click ? manageClick : restartCounter}>  
         {text}
         </button>
